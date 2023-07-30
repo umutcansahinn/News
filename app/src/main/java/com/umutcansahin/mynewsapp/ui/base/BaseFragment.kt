0d1bce9c41f21ad6abeaa76c47664b667640c0ac
Umutcan Sahin <umutcan199726@gmail.com>
2023-07-30 18:24:49 +0300

    fix binding visibility modifier from private to public

--- app/src/main/java/com/umutcansahin/mynewsapp/ui/base/BaseFragment.kt
+++ app/src/main/java/com/umutcansahin/mynewsapp/ui/base/BaseFragment.kt
@@ -12,7 +12,7 @@ abstract class BaseFragment<VB : ViewBinding>(
 ) : Fragment() {
 
     private var _binding: VB? = null
-    private val binding get() = _binding!!
+    val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
